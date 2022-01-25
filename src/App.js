587cc79b59f8a8829bce846df601c5dd207188db
Nathan Piper <nathanpiper22@gmail.com>
2022-01-24 23:08:24 -0800

    set up skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from './components/Header.js'
+import Header from './components/Main.js'
+
+function App() {
+  return (
+    <div className="container">
+      <Main />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import GamesList from './GamesList'
+
+export default function Main() {
+    return (
+        <div>
+            <GamesList />
+        </div>
+    )
+}
