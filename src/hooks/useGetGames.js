fbb3dd53880fb08385d6dc9f8f4e18e5f9f26ad7
Nathan Piper <nathanpiper22@gmail.com>
2022-02-01 20:19:12 -0800

    added quick check to see if there were anymore pages to display before making another api call

--- src/hooks/useGetGames.js
+++ src/hooks/useGetGames.js
@@ -1,47 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const filterGames = (games) => {
-    const titles = [];
-    const filteredGames = [];
-    for(let i=0; i < games.length; i++) {
-        if(!titles.includes(games[i].title))
-            filteredGames.push(games[i]);
-        titles.push(games[i].title)
-    }
-    return filteredGames;
-} 
-
+const filterGames = games => {
+  const titles = [];
+  const filteredGames = [];
+  for (let i = 0; i < games.length; i++) {
+    if (!titles.includes(games[i].title)) filteredGames.push(games[i]);
+    titles.push(games[i].title);
+  }
+  return filteredGames;
+};
 
 export default function useGetGames(options, query, pageNumber) {
+  const [loading, setLoading] = useState(true);
+  const [games, setGames] = useState([]);
+  const [error, setError] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
 
-    const [loading, setLoading] = useState(true);
-    const [games, setGames] = useState([]);
-    const [error, setError] = useState(null);
+  useEffect(() => {
+    setGames([]);
+    setHasMore(true);
+  }, [query, options]);
 
-    useEffect(() => {
-        setGames([]);
-    }, [query, options])
-
-    useEffect(() => {
-        let controller = new AbortController();
-        setLoading(true);
-        axios({
-            method: 'GET',
-            url: 'https://www.cheapshark.com/api/1.0/deals',
-            params: {...options.options, title: query, pageNumber: pageNumber},
-            signal: controller.signal
-        }).then(res => {
-            setGames(prevGames => {
-                return filterGames([...prevGames, ...res.data]);
-            })
-            setLoading(false);
-        }).catch( err => {
-            setLoading(false);
-            setError(err);
+  useEffect(() => {
+    let controller = new AbortController();
+    setLoading(true);
+    if (hasMore) {
+      axios({
+        method: "GET",
+        url: "https://www.cheapshark.com/api/1.0/deals",
+        params: { ...options.options, title: query, pageNumber: pageNumber },
+        signal: controller.signal
+      })
+        .then(res => {
+          console.log(res.headers["x-total-page-count"]);
+          if (pageNumber >= res.headers["x-total-page-count"]) {
+            setHasMore(false);
+          } else {
+            setHasMore(true);
+          }
+          setGames(prevGames => {
+            return filterGames([...prevGames, ...res.data]);
+          });
+          console.log(res);
+          setLoading(false);
         })
-        return () => controller.abort();
-    }, [options, query, pageNumber])
+        .catch(err => {
+          setLoading(false);
+          setError(err);
+        });
+    }
+    return () => controller.abort();
+  }, [options, query, pageNumber]);
 
-  return {loading, games, error};
+  return { loading, games, error };
 }
