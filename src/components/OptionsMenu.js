c43f59facf42a5f97afc55a80dbce17f71ad14ce
Nathan Piper <nathanpiper22@gmail.com>
2022-02-01 18:02:13 -0800

    setup base styles for header and options menu as well as added basic options menu values

--- src/components/OptionsMenu.js
+++ src/components/OptionsMenu.js
@@ -1,24 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function OptionsMenu() {
+  const [priceRange] = useState([0, 61]);
+  const [savingsRange] = useState([0, 100]);
+  const [currentPrice, setCurrentPrice] = useState(priceRange[1]);
+  const [currentSavings, setCurrentSavings] = useState(savingsRange[0]);
+  const [hiding, setHiding] = useState(true);
+
+  const handlePriceChange = e => {
+    setCurrentPrice(e.target.value);
+  };
+  const handleSavingsChange = e => {
+    setCurrentSavings(e.target.value);
+  };
+
+  const toggleHide = () => {
+    setHiding(prev => !prev);
+  }
+
+  if(hiding){
+    return (
+      <button className="filter-button" onClick={toggleHide}>Filter</button>
+    )
+  }
   return (
     <div className="options-menu">
-      <label htmlFor="store-select">Store:</label>
-      <select name="store-select" id="store-select">
-      <option value="steam">Steam</option>
-      <option value="epic">Epic</option>
-      <option value="gog">GOG</option>
-      <option value="greenman">Greenman Gaming</option>
-      </select>
+      <div className="options-item">
+        <label htmlFor="store-select">Store</label>
+        <select name="store-select" id="store-select">
+          <option value="steam">Steam</option>
+          <option value="epic">Epic</option>
+          <option value="gog">GOG</option>
+          <option value="greenman">Greenman Gaming</option>
+        </select>
+      </div>
 
-      <label htmlFor="price-range">Price:</label>
-      <input type="range" id="price-range"></input>
+      <div className="options-item">
+        <label htmlFor="price-range">Price: {currentPrice == priceRange[1] ? `<$${priceRange[1]-1}` : `$${currentPrice}`}</label>
+        <div className="range-values">
+          <span className="range-minmax">${priceRange[0]}</span>
+          <input
+            className="slider"
+            id="price-range"
+            type="range"
+            min={priceRange[0].toString()}
+            max={priceRange[1].toString()}
+            value={currentPrice}
+            steps={1}
+            onChange={handlePriceChange}
+          ></input>
+          <span className="range-minmax">{currentPrice == priceRange[1] ? `<$${priceRange[1]-1}` : `$${currentPrice}`}</span>
+        </div>
+      </div>
 
-      <label htmlFor="release-range">Release Date</label>
-      <input type="range" id="release-range"></input>
-
-      <label htmlFor="savings-range">Savings</label>
-      <input type="range" id="savings-range"></input>
+      <div className="options-item">
+        <label htmlFor="savings-range">Savings: {currentSavings}%</label>
+        <div className="range-values">
+          <span className="range-minmax">{savingsRange[0]}%</span>
+          <input
+            className="slider"
+            id="savings-range"
+            type="range"
+            min={savingsRange[0].toString()}
+            max={savingsRange[1].toString()}
+            value={currentSavings}
+            steps={5}
+            onChange={handleSavingsChange}
+          ></input>
+          <span className="range-minmax">{savingsRange[1]}%</span>
+        </div>
+      </div>
+      <button className="filter-button hide-button" onClick={toggleHide}>Hide</button>
     </div>
   );
 }
