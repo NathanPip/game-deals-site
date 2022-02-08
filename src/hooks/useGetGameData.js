76db6ec5bb3bbe033b057f4fd7828add01ce40fc
Nathan Piper <nathanpiper22@gmail.com>
2022-02-08 12:51:20 -0800

    removed local testing

--- src/hooks/useGetGameData.js
+++ src/hooks/useGetGameData.js
@@ -24,7 +24,7 @@ export default function useGetGameData(game) {
         setLoading(true);
         axios({
           method: "GET",
-          url: `http://localhost:3000/steamData?steamID=${game.steamAppID}`,
+          url: `/steamData?steamID=${game.steamAppID}`,
           signal: controller.signal
         })
           .then(res => {
