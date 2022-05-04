8e042f8e51b666851c20b443f12eb9d407c508f4
Nathan Piper <nathanpiper22@gmail.com>
2022-05-04 16:38:38 -0700

    configure jest to work with ECMAScript modules

---
+++ client/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+};
---
+++ client/src/tests/helper.test.js
@@ -0,0 +1,2 @@
+import { convertIDs } from '../helpers/helperFunctions';
+
